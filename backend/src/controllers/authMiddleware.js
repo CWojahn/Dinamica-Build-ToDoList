91941e90685cc3393d7842a92669ea2a9cd9cdba
Carlos W <carloswojahn@gmail.com>
2020-04-29 06:27:06 -0300

    AuthMiddleware corrected

--- backend/src/controllers/authMiddleware.js
+++ backend/src/controllers/authMiddleware.js
@@ -11,7 +11,7 @@ const authMiddleWare = async (request, response, next) => {
 			.where('idusuarios', payload.userId)
 			.first();
 
-		if (!user) {
+		if (user.length < 1) {
 			return response.sendStatus(401);
 		}
 		request.auth = user;
